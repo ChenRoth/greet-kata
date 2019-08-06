6de5ad7bb9172ed2b42d47595187b83e163574f5
noUser <noUser>
2019-08-06 21:09:17 +0300

    solve 6 requirements of the greet kata https://github.com/testdouble/contributing-tests/wiki/Greeting-Kata

--- app.test.ts
+++ app.test.ts
@@ -1,13 +1,39 @@
-import { sum } from './app';
+import { greet, splitNames } from './app';
 
-describe('sum', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3);
+describe('greet', () => {
+    it('should say hello to a given person', () => {
+        expect(greet('Bob')).toBe('Hello, Bob.');
+        expect(greet('Jane')).toBe('Hello, Jane.');
     });
 
-    it('should return a correct sum', () => {
-        expect(sum(5, 0)).toBe(5);
+    it('should use a default message if no name is provided', () => {
+        expect(greet(null)).toBe('Hello, my friend.');
+    });
+
+    it('should shout back upon receiving an uppercased name', () => {
+        expect(greet('JERRY')).toBe('HELLO JERRY!');
+    });
+
+    it('should say hello to 2 people', () => {
+        expect(greet(['Jill', 'Jane'])).toBe('Hello, Jill and Jane.');
+    });
+
+    it('should say hello to 2 crazy people', () => {
+        expect(greet(['JILL', 'JANE'])).toBe('HELLO JILL AND JANE!');
+    });
+
+    it('should say hello to more than 2 people', () => {
+        expect(greet(['Amy', 'Brian', 'Charlotte'])).toBe('Hello, Amy, Brian and Charlotte.');
+    });
+
+    it('should allow a mix of normal and shouted names', () => {
+        expect(greet(["Amy", "BRIAN", "Charlotte", "JOHN"])).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN AND JOHN!');
     });
 });
 
-
+describe('splitNames', () => {
+    it('should split to shouted and normal groups', () => {
+        expect(splitNames(['amy', 'BRIAN', 'charlotte'])).toStrictEqual([['amy', 'charlotte'], ['BRIAN']]);
+        expect(splitNames(['BRIAN', 'amy', 'CHARLOTTE'])).toStrictEqual([['amy'], ['BRIAN', 'CHARLOTTE']]);
+    });
+});
--- app.ts
+++ app.ts
@@ -1,3 +1,50 @@
-export function sum(a, b) {
-    return a + b;
+export function greet(input: string | string[]): string {
+    if (input instanceof Array) {
+        const [normal, shouted] = splitNames(input);
+        const firstSentence = handleNormalPeople(normal);
+        const secondSentence = handleCrazyPeople(shouted);
+
+        if (!firstSentence) {
+            return secondSentence;
+        } else if (!secondSentence) {
+            return firstSentence;
+        }
+        return [firstSentence, secondSentence].join(' AND ');
+    }
+    return isUppercase(input as string) ? `HELLO ${input}!` : `Hello, ${input || 'my friend'}.`;
+}
+
+function isUppercase(text: string): boolean {
+    return text && text === text.toUpperCase();
+}
+
+function handleNormalPeople(names: string[]): string {
+    if (!names.length) {
+        return '';
+    }
+    const lastName = names[names.length - 1];
+    const allNamesExceptLast = names.slice(0, names.length - 1);
+    return `Hello, ${allNamesExceptLast.join(', ')} and ${lastName}.`
+}
+
+function handleCrazyPeople(names: string[]): string {
+    if (!names.length) {
+        return '';
+    }
+    const lastName = names[names.length - 1];
+    const allNamesExceptLast = names.slice(0, names.length - 1);
+    return `HELLO ${allNamesExceptLast.join(', ')} AND ${lastName}!`
+}
+
+export function splitNames(names: string[]): [string[], string[]] {
+    const shouted: string[] = [];
+    const normal: string[] = [];
+    names.forEach(name => {
+        if (isUppercase(name)) {
+            shouted.push(name);
+        } else {
+            normal.push(name);
+        }
+    });
+    return [normal, shouted];
 }
